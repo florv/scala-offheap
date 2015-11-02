6ccf322764cb170a832440b56fc4ad59c112d2b0
Florian Vessaz <florian@florv.ch>
2015-11-02 07:25:38 +0100

    Add methods and partial implementation

--- core/src/main/scala/offheap/numeric/DenseMatrix.scala
+++ core/src/main/scala/offheap/numeric/DenseMatrix.scala
@@ -3,6 +3,10 @@ package scala.offheap.numeric
 import scala.offheap._
 import scala.offheap.numeric.jni._
 
+// TODO:
+//   - Don't assume stride size match (for instance when copying)
+//   - Methods to extract submatrices
+
 class DenseMatrix private (val addr: Addr) extends AnyVal {
 
   private def dataAddr = addr + 3 * strideOf[Int]
@@ -27,6 +31,25 @@ class DenseMatrix private (val addr: Addr) extends AnyVal {
     dataAddr + row * strideOf[Double] + stride * strideOf[Double] * column
   }
 
+  private def inplaceOpNoAlloc(op: DenseMatrix, f: (Double, Double) => Double, res: DenseMatrix): Unit = {
+    if (op.rows != rows || op.columns != columns) {
+      throw new IllegalArgumentException("Matrices dimensions don't match: "
+        + "left operand is " + rows + "×" + columns
+        + "while right operand is " + op.rows + "×" + op.columns)
+    }
+    for (col <- 0 until columns) {
+      for (row <- 0 until rows) {
+        res(row, col) = f(apply(row, col), op(row, col))
+      }
+    }
+  }
+
+  private def inplaceOp(op: DenseMatrix, f: (Double, Double) => Double)(implicit a: Allocator): DenseMatrix = {
+    val res = DenseMatrix.uninit(rows, columns)
+      inplaceOpNoAlloc(op, f, res)
+    res
+  }
+
   def apply(row: Int, column: Int): Double = {
     Memory.getDouble(addrOf(row, column))
   }
@@ -35,6 +58,27 @@ class DenseMatrix private (val addr: Addr) extends AnyVal {
     Memory.putDouble(addrOf(row, column), value)
   }
 
+  def updateAll(f: (Int, Int) => Double): Unit = {
+    for (col <- 0 until columns) {
+      for (row <- 0 until columns) {
+        update(row, col, f(row, col))
+      }
+    }
+  }
+
+  /** Return true if all elements of the matrices are pairwise equal */
+  def ==(m: DenseMatrix): Boolean = {
+    for (col <- 0 until columns) {
+      for (row <- 0 until rows) {
+        if (apply(row, col) != m(row, col)) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+
+  /** Matrix multiplication */
   def *(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
     val res = DenseMatrix.uninit(rows, m.columns);
     val transa: Int = LapackJNI.CblasNoTrans
@@ -48,20 +92,279 @@ class DenseMatrix private (val addr: Addr) extends AnyVal {
     res
   }
 
-  def +(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
-    if (m.rows != rows || m.columns != columns) {
-      throw new IllegalArgumentException("Matrices dimensions dont match: "
-        + "left operand is " + rows + "×" + columns
-        + "while right operand is " + m.rows + "×" + m.columns)
+  /** Dot product
+   *
+   * This method automatically transpose the argument if necessary. */
+  def dot(m: DenseMatrix)(implicit a: Allocator): Double = {
+    if (columns != 1 && rows != 1) {
+      throw new IllegalArgumentException("This matrix is not a vector")
     }
-    val res = DenseMatrix.uninit(rows, columns)
-    for (c <- 0 until columns) {
-      for (r <- 0 until rows) {
-        res.update(r, c, apply(r, c) + m(r, c))
+    if (m.columns != 1 && rows != 1) {
+      throw new IllegalArgumentException("The argument is not a vector")
+    }
+    val n = if (columns != 1) columns else rows
+    if (m.columns != n && m.rows != n) {
+      throw new IllegalArgumentException("The vector sizes don't match")
+    }
+    val incx = if (columns == 1) 1 else stride
+    val incy = if (m.columns == 1) 1 else m.stride
+    LapackJNI.cblas_ddot(n, dataAddr, incx, m.dataAddr, incy)
+  }
+
+  /** Element-wise addition */
+  def +(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => a + b)
+  }
+
+  /** In-place element-wise addition */
+  def +=(m: DenseMatrix): Unit = {
+    inplaceOpNoAlloc(m, (a, b) => a + b, this)
+  }
+
+  /** Element-wise multiplication */
+  def :*(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => a * b)
+  }
+
+  /** Element-wise division */
+  def :/(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => a / b)
+  }
+
+  /** Element-wise &lt; comparison */
+  def :<(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => if (a < b) 1.0 else 0.0)
+  }
+
+  /** Element-wise &lt;= comparison */
+  def :<=(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => if (a <= b) 1.0 else 0.0)
+  }
+
+  /** Element-wise &gt; comparison */
+  def :>(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => if (a > b) 1.0 else 0.0)
+  }
+
+  /** Element-wise &gt;= comparison */
+  def :>=(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => if (a >= b) 1.0 else 0.0)
+  }
+
+  /** Element-wise == comparison */
+  def :==(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => if (a == b) 1.0 else 0.0)
+  }
+
+  /** In-place addition (mutates this) */
+  def :+=(v: Double): Unit = {
+    inplace(x => x + v)
+  }
+
+  /** In-place multiplication (mutates this) */
+  def :*=(v: Double): Unit = {
+    inplace(x => x * v)
+  }
+
+  /** Element-wise &amp; */
+  def :&(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => if (a != 0.0 && b != 0.0) 1.0 else 0.0)
+  }
+
+  /** Element-wise | */
+  def :|(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    inplaceOp(m, (a, b) => if (a != 0.0 || b != 0.0) 1.0 else 0.0)
+  }
+
+  /** Element-wise not */
+  def !()(implicit a: Allocator): DenseMatrix = {
+    val res = copy
+    res.inplace(a => if (a == 0.0) 1.0 else 0.0)
+    res
+  }
+
+  /** Return true if any element is non-zero */
+  def any(): Boolean = {
+    for (col <- 0 until columns) {
+      for (row <- 0 until rows) {
+        if (apply(row, col) != 0.0) {
+          return true
+        }
       }
+    }
+    return false
+  }
+
+  /** Return true if all elements are non-zero */
+  def all(): Boolean = {
+    for (col <- 0 until columns) {
+      for (row <- 0 until rows) {
+        if (apply(row, col) == 0.0) {
+          return false
+        }
+      }
+    }
+    return true
+  }
+
+  /** Apply the given function on each element, in-place. */
+  def inplace(f: Double => Double): Unit = {
+    for (col <- 0 until columns) {
+      for (row <- 0 until columns) {
+        update(row, col, f(apply(row, col)))
+      }
+    }
+  }
+
+  def round()(implicit a: Allocator): DenseMatrix = {
+    val res = copy
+    res.inplace(math.round(_).toDouble)
+    res
+  }
+
+  def ceil()(implicit a: Allocator): DenseMatrix = {
+    val res = copy
+    res.inplace(math.ceil)
+    res
+  }
+
+  def floor()(implicit a: Allocator): DenseMatrix = {
+    val res = copy
+    res.inplace(math.floor)
+    res
+  }
+
+  def signum()(implicit a: Allocator): DenseMatrix = {
+    val res = copy
+    inplace(math.signum)
+    res
+  }
+
+  def abs()(implicit a: Allocator): DenseMatrix = {
+    val res = copy
+    inplace(math.abs)
+    res
+  }
+
+  /** Find max */
+  def max(): Double = {
+    ???
+  }
+
+  /** Find min */
+  def min(): Double = {
+    ???
+  }
+
+  /** Coordinates of the maximum */
+  def argmax(): (Int, Int) = {
+    ???
+  }
+
+  /** Coordinates of the minimum */
+  def argmin(): (Int, Int) = {
+    ???
+  }
+
+  /** Sum of the elemens */
+  def sum(): Double = {
+    var s: Double = 0
+    for (col <- 0 until columns) {
+      for (row <- 0 until rows) {
+        s += apply(row, col)
+      }
+    }
+    s
+  }
+
+  /** Trace of the matrix */
+  def trace(): Double = {
+    var t: Double = 0
+    for (i <- 0 until math.max(rows, columns)) {
+      t += apply(i, i)
+    }
+    t
+  }
+
+  /** Cumulative sum */
+  def accumulate(): Double = {
+    ???
+  }
+
+  /** Return a transposed copy of the matrix. */
+  def t(): DenseMatrix = {
+    ???
+  }
+
+  /** Return a copy of the matrix. */
+  def copy()(implicit a: Allocator): DenseMatrix = {
+    val m = DenseMatrix.uninit(rows, columns)
+    Memory.copy(dataAddr, m.dataAddr, columns * m.stride * strideOf[Double])
+    m
+  }
+
+  /** Copy the lower triangular portion in a new matrix. */
+  def lowerTriangular()(implicit a: Allocator): DenseMatrix = {
+    ???
+  }
+
+  /** Copy the upper triangular portion in a new matrix. */
+  def upperTriangular()(implicit a: Allocator): DenseMatrix = {
+    ???
+  }
+
+  /** Concatenate a matrix underneath. */
+  def vertcat(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    ???
+  }
+
+  /** Concatenate a matrix on the right. */
+  def horzcat(m: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    if (m.rows != rows) {
+      throw new IllegalArgumentException("Number of rows do not match: " + rows +
+        "×" + columns + "on the left while " + m.rows + "×" + m.columns + "on the right")
+    }
+    val res = DenseMatrix.uninit(rows, columns + m.columns)
+    if (m.stride == stride && res.stride == stride) {
+      val len1 = columns * stride * strideOf[Double]
+      val len2 = m.columns * m.stride * strideOf[Double]
+      Memory.copy(dataAddr, res.dataAddr, len1)
+      Memory.copy(m.dataAddr, res.dataAddr + len1, len2)
+    } else {
+      /* Copy each column individually */
+      ???
     }
     res
   }
+
+  /** Solve linear system
+   *
+   * Solve this * x = b and return x */
+  def \(b: DenseMatrix)(implicit a: Allocator): DenseMatrix = {
+    ???
+  }
+
+  /** Determinant */
+  def det(): Double = {
+    ???
+  }
+
+  /** Matrix inverse */
+  def inv()(implicit a: Allocator): DenseMatrix = {
+    ???
+  }
+
+  /** Moore-Penrose pseudoinverse */
+  def pinv()(implicit a: Allocator): DenseMatrix = {
+    ???
+  }
+
+  /** Vector frobenius norm */
+  def norm(): Double = {
+    ???
+  }
+
+  /* TODO: Eigenvectors and eigenvalues */
 }
 
 object DenseMatrix {
@@ -94,12 +397,59 @@ object DenseMatrix {
     val stride = rows
     val m = fromAddr(addr)
     m.setMetadata(rows, columns, stride)
-    return m
+    m
   }
 
+  /** Create a matrix of zeroes. */
   def zeros(rows: Int, columns: Int)(implicit a: Allocator): DenseMatrix = {
     val m = uninit(rows, columns)
     Memory.zero(m.dataAddr, columns * m.stride * strideOf[Double])
-    return m
+    m
   }
+
+  /** Create a vector of zeroes. */
+  def zeros(rows: Int)(implicit a: Allocator): DenseMatrix = {
+    zeros(rows, 1)
+  }
+
+  /** Create a vector of ones. */
+  def ones(rows: Int)(implicit a: Allocator): DenseMatrix = {
+    val m = uninit(rows, 1)
+    m.inplace(_ => 1)
+    m
+  }
+
+  /** Create a square matrix with the given elements on the diagonal. */
+  def diag(d: List[Double])(implicit a: Allocator): DenseMatrix = {
+    var m = uninit(d.size, d.size)
+    for (i <- 0 until d.size) {
+      m(i, i) = d(i)
+    }
+    m
+  }
+
+  /** Create a new matrix, filling it with the result of the given function. */
+  def tabulate(rows: Int, columns: Int)(f: (Int, Int) => Double)(implicit a: Allocator): DenseMatrix = {
+    var m = uninit(rows, columns)
+    m.updateAll(f)
+    m
+  }
+
+  /** Create a new vector, filling it with the result of the given function. */
+  def tabulate(rows: Int)(f: Int => Double)(implicit a: Allocator): DenseMatrix = {
+    tabulate(rows, 1)((r, c) => f(r))
+  }
+
+  /** Create a matrix of random elements in the range [0, 1]. */
+  def rand(rows: Int, columns: Int)(implicit a: Allocator): DenseMatrix = {
+    var m = uninit(rows, columns)
+    m.updateAll((_, _) => math.random)
+    m
+  }
+
+  /** Create a vector of random elements in the range [0, 1]. */
+  def rand(rows: Int)(implicit a: Allocator): DenseMatrix = {
+    rand(rows, 1)
+  }
+
 }
