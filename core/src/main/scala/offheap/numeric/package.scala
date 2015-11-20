316b237e8b02e58ab805f5f4695feeac381a7ad1
Denys Shabalin <denys.shabalin@epfl.ch>
2015-11-20 15:08:09 +0100

    Matrix optimise prototype
    Conflicts:
    	core/src/main/scala/offheap/numeric/package.scala

--- core/src/main/scala/offheap/numeric/package.scala
+++ core/src/main/scala/offheap/numeric/package.scala
@@ -1,6 +1,12 @@
 package scala.offheap
 
+import scala.offheap.internal.macros
+
 package object numeric extends OffheapPackage {
+  import scala.language.experimental.{macros => canMacro}
+
+  def opt[T](t: T): T = macro macros.NumericMethod.opt
+
   implicit def Double2DenseMatrixRichDouble(value: Double): DenseMatrixRichDouble =
     new DenseMatrixRichDouble(value)
 }
